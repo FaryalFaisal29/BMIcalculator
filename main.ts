625e35df90cf22565dc3083ba20f12c7113adb53
FaryalFaisal29 <164797311+FaryalFaisal29@users.noreply.github.com>
2024-03-26 01:22:02 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,12 @@
+let weightInKg = 70;
+let heightInMeters = 1.8;
+let bmi = weightInKg/(heightInMeters*heightInMeters)
+
+console.log(`Your BMI is ${bmi}`);
+
+let myWeight = 62;
+let myHeight = 1.85;
+let mybmi = myWeight/(myHeight*myHeight)
+
+console.log(`Your BMI is ${mybmi}`);
+
